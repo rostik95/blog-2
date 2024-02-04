79a63d9999ce6d5b76cf58c6da7abfe59bb3b7d4
rostik95 <danver1995@mail.ru>
2024-02-04 16:24:21 +0300

    added first django project

---
+++ mysite/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse('<h1>My first http response</h1>')
+
+
+def hello(request):
+    return HttpResponse("Hello<br>World!!!")
