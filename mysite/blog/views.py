ef17ef474af9f1e835daefb67e08470df501b0a6
rostik95 <danver1995@mail.ru>
2024-02-25 14:36:26 +0300

    added views to blog app

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {"posts": posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {"post", post})
+
 
 
 # Create your views here.
