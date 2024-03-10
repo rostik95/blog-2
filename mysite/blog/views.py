756897d905122122648f11ff0ed56168142647b7
rostik95 <danver1995@mail.ru>
2024-03-10 15:27:56 +0300

    add comments

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,32 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Comment
+from django.views.generic import ListView
+
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post,
+                                                      'form': form,
+                                                      'comment': comment})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -24,7 +49,11 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day,
                              slug=post)
-    return render(request, 'blog/post/detail.html', {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {"post": post,
+                                                     'comments': comments,
+                                                     'form': form})
 
 
 
