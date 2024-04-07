1f33aca0833f7f44e3d835d64037bc6e9e4e9f08
rostik95 <danver1995@mail.ru>
2024-04-07 15:26:57 +0300

    added user logic

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
